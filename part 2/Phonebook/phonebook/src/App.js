a7fbe95a37ae7a6aada88091b5e0a68c3cbb8e29
Yuuuusssuufff <ajibolayusuf8@gmail.com>
2022-10-06 11:48:35 +0100

    node init

--- part 2/Phonebook/phonebook/src/App.js
+++ part 2/Phonebook/phonebook/src/App.js
@@ -7,15 +7,15 @@ import Filter from "./components/Filter";
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    // { name: "Arto Hellas", number: "040-123456", id: 1 },
+    // { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    // { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    // { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchKey, setSearchKey] = useState("");
-  const url = "http://localhost:3000/persons";
+  const url = "http://localhost:3001/persons";
 
   useEffect(() => {
     axios.get(url).then((res) => setPersons(res.data));
